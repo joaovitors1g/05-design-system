44cffed0f54ee11cdd139ab2695715594b7d7230
joaovitors1g <joao_vitor9846@hotmail.com>
2023-08-20 19:56:01 -0300

    feat(docs): add button stories

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    children: 'Enviar',
+  },
+} as Meta<ButtonProps>
+
+type Story = StoryObj<ButtonProps>
+
+export const Primary: Story = {}
+
+export const Big: Story = {
+  args: {
+    size: 'big',
+  },
+}
