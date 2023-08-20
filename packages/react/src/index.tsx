c05250aa641b72d94a7ace26ce35661449bf8576
joaovitors1g <joao_vitor9846@hotmail.com>
2023-08-20 19:55:33 -0300

    feat: add basic button component

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,3 +1,30 @@
-export function App() {
-  return <></>
-}
+import { ComponentProps } from 'react'
+import { styled } from '../styles'
+
+export const Button = styled('button', {
+  backgroundColor: '$ignite300',
+  border: 'none',
+  fontWeight: '$bold',
+  borderRadius: '$sm',
+  fontFamily: '$default',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
+})
+
+export type ButtonProps = ComponentProps<typeof Button>
