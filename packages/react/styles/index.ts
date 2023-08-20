5a1628e0808beff5f9f56da8f3f999bb0d6d1f02
joaovitors1g <joao_vitor9846@hotmail.com>
2023-08-19 21:39:57 -0300

    feat: add initial structure

---
+++ packages/react/styles/index.ts
@@ -0,0 +1,28 @@
+import { createStitches, defaultThemeMap } from '@stitches/react'
+import * as tokens from '@ignite-ui/tokens'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+  },
+  theme: {
+    colors: tokens.colors,
+    space: tokens.space,
+    fonts: tokens.fonts,
+    fontSizes: tokens.fontSizes,
+    fontWeights: tokens.fontWeights,
+    lineHeights: tokens.lineHeights,
+    radii: tokens.radii,
+  },
+})
---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,13 @@
+export * from './colors'
+
+export * from './fonts'
+
+export * from './font-sizes'
+
+export * from './font-weights'
+
+export * from './line-heights'
+
+export * from './radii'
+
+export * from './space'
