8b3458d6a4852a2307273edc6d30f9e2055d7c0f
joaovitors1g <joao_vitor9846@hotmail.com>
2023-09-03 23:30:22 -0300

    docs: add Text story

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto, est! Sunt, magni pariatur sit molestias eos architecto consequuntur rerum, mollitia facere corporis maxime provident quisquam nemo minima quos maiores veniam.',
+  },
+} as Meta<TextProps>
+
+type Story = StoryObj<TextProps>
+
+export const Primary: Story = {}
+
+export const CustomTag: Story = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
