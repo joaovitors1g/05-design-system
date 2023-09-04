d8399eb66acbfdb0974a7453420e2064cdb48bc6
joaovitors1g <joao_vitor9846@hotmail.com>
2023-09-03 23:30:37 -0300

    feat(react): add Heading component

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -2,6 +2,7 @@ import { ElementType } from 'react'
 
 export * from './components/Box'
 export * from './components/Text'
+export * from './components/Heading'
 
 export type WithAs = {
   as?: ElementType
