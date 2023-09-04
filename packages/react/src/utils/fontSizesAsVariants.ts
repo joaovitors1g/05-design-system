4b2a09c0abdf869ff29027c8bc5a64455040a82a
joaovitors1g <joao_vitor9846@hotmail.com>
2023-09-03 23:29:55 -0300

    feat(react): add font size variants utility

---
+++ packages/react/src/utils/fontSizesAsVariants.ts
@@ -0,0 +1,15 @@
+export const fontSizesAsVariants = {
+  '2xs': { fontSize: '$2xs' },
+  xs: { fontSize: '$xs' },
+  sm: { fontSize: '$sm' },
+  md: { fontSize: '$md' },
+  lg: { fontSize: '$lg' },
+  xl: { fontSize: '$xl' },
+  '2xl': { fontSize: '$2xl' },
+  '4xl': { fontSize: '$4xl' },
+  '5xl': { fontSize: '$5xl' },
+  '6xl': { fontSize: '$6xl' },
+  '7xl': { fontSize: '$7xl' },
+  '8xl': { fontSize: '$8xl' },
+  '9xl': { fontSize: '$9xl' },
+}
