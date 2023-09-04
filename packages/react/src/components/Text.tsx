bffa784a3403b818cc4c6f2283a5909ed676147c
joaovitors1g <joao_vitor9846@hotmail.com>
2023-09-03 23:30:14 -0300

    feat(react): add Text component

---
+++ packages/react/src/components/Text.tsx
@@ -0,0 +1,23 @@
+import { ComponentProps } from 'react'
+
+import { WithAs } from '..'
+
+import { styled } from '../../styles'
+import { fontSizesAsVariants } from '../utils/fontSizesAsVariants'
+
+export const Text = styled('p', {
+  fontFamily: '$default',
+  lineHeight: '$base',
+  margin: 0,
+  color: '$gray100',
+
+  variants: {
+    size: fontSizesAsVariants,
+  },
+
+  defaultVariants: {
+    size: 'md',
+  },
+})
+
+export type TextProps = ComponentProps<typeof Text> & WithAs
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,6 +1,7 @@
 import { ElementType } from 'react'
 
 export * from './components/Box'
+export * from './components/Text'
 
 export type WithAs = {
   as?: ElementType
