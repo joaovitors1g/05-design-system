57b931d8ed4278a1b92b32d8b227134bec294bcd
joaovitors1g <joao_vitor9846@hotmail.com>
2023-09-05 22:39:27 -0300

    feat(react): add Button component

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -4,6 +4,7 @@ export * from './components/Box'
 export * from './components/Text'
 export * from './components/Heading'
 export * from './components/Avatar'
+export * from './components/Button'
 
 export type WithAs = {
   as?: ElementType
