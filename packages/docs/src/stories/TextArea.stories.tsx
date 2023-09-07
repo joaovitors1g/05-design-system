850c82624b14e74849a172e94f1f306047959a57
joaovitors1g <joao_vitor9846@hotmail.com>
2023-09-07 17:24:49 -0300

    feat(docs): add TextArea stories

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, Text, TextArea, TextAreaProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Text Area',
+  component: TextArea,
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '$2',
+          }}
+        >
+          <Text size="sm">Description</Text>
+          <Story />
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextAreaProps>
+
+type Story = StoryObj<TextAreaProps>
+
+export const Primary: Story = {
+  args: {
+    placeholder: 'Cool description here',
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+}
