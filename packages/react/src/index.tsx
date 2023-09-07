b4527675a6a602425057a742ceaa9866ae3f81f7
joaovitors1g <joao_vitor9846@hotmail.com>
2023-09-07 17:25:35 -0300

    feat(react): add Checkbox component

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -7,6 +7,7 @@ export * from './components/Avatar'
 export * from './components/Button'
 export * from './components/TextInput'
 export * from './components/TextArea'
+export * from './components/Checkbox'
 
 export type WithAs = {
   as?: ElementType
