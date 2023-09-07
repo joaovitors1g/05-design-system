789706744fddc4e798d61cfd57abf55cf27d7520
joaovitors1g <joao_vitor9846@hotmail.com>
2023-09-07 17:24:34 -0300

    feat(react): add TextArea component

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -6,6 +6,7 @@ export * from './components/Heading'
 export * from './components/Avatar'
 export * from './components/Button'
 export * from './components/TextInput'
+export * from './components/TextArea'
 
 export type WithAs = {
   as?: ElementType
